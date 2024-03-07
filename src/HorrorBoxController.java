9763e3eba7fc9b76c37f1e7bcf339065a7fb938d
Bryons-factory <115902848+Bryons-factory@users.noreply.github.com>
2024-03-07 11:50:54 -0600

    HorrorBox Controller written

---
+++ src/HorrorBoxController.java
@@ -0,0 +1,30 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
+public class HorrorBoxController {
+
+    @FXML
+    private Button cry;
+
+    @FXML
+    private Button leftPath;
+
+    @FXML
+    private Label narrator;
+
+    @FXML
+    private ImageView pathPhoto;
+
+    @FXML
+    private Button rightPath;
+
+    @FXML
+    private Button straightPath;
+
+    @FXML
+    private TextArea userTxt;
+
+}
