bf1b36de40628783761a4d9806b95f2e80e3e4fd
Bryons-factory <115902848+Bryons-factory@users.noreply.github.com>
2024-03-07 11:32:51 -0600

    PURGE!!!!

--- HorrorSceneController.java
+++
@@ -1,9 +0,0 @@
-//import javafx.fxml.FXML;
-//import javafx.scene.control.Label;
-//import javafx.scene.control.Slider;
-//import javafx.scene.control.TextField;
-//HorrorSceneController.java, should control HorrorScene.fxml
-//This should use chapter 12 and 13 notes
-public class HorrorSceneController {
-
-}
