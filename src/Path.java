c231d297b85f6a36fc52db7bacd8f2a53546e791
Bryons-factory <115902848+Bryons-factory@users.noreply.github.com>
2024-03-12 23:00:42 -0500

    Piece of Written story
    I will continue to update for each potential path.

---
+++ src/Path.java
@@ -0,0 +1,16 @@
+
+public abstract class Path {
+	//This Counter will keep track of how far along the player is in the story
+	int counter = 0;
+	
+	//This will be the area for generating content for their journey
+	public String toString() {
+		return " "; 
+	}
+	
+	// This is the constructor where the counter is stored.
+	Path(int count) {
+		this.counter = count;
+	}
+
+}
