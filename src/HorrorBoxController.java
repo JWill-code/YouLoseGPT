125f07cd5b9daa889e5bfdf9cda87d3a9aca46db
MattSemrau <158501385+MattSemrau@users.noreply.github.com>
2024-03-13 20:16:31 -0500

    Minor Changes
    Added a folder for all images to be used, created onMouseClicked for the labels.

--- src/HorrorBoxController.java
+++ src/HorrorBoxController.java
@@ -2,21 +2,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class HorrorBoxController {
 
     @FXML
-    private Button cry;
-
-    @FXML
     private Button leftPath;
-
-    @FXML
-    private Label narrator;
-
-    @FXML
-    private ImageView pathPhoto;
 
     @FXML
     private Button rightPath;
@@ -25,6 +17,39 @@ public class HorrorBoxController {
     private Button straightPath;
 
     @FXML
+    private Button cry;
+
+    @FXML
     private TextArea userTxt;
+
+    @FXML
+    private ImageView pathPhoto;
+
+    @FXML
+    private Label narrator;
+
+    Image gameOver = new Image("images/GameOVer.jpg");
+    
+    
+    
+    @FXML
+    void cryButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void walkLeftButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void walkRightButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void walkStraightButton(MouseEvent event) {
+
+    }
 
 }
