c2335c5a0077ba2633e568e42927cd7227b891db
Bryons-factory <115902848+Bryons-factory@users.noreply.github.com>
2024-03-16 17:45:21 -0500

    Added Comments

--- AdvSoftwareMidterm/src/End.java
+++ AdvSoftwareMidterm/src/End.java
@@ -5,7 +5,8 @@ public class End extends Path {
 		super(count);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	//Universal toString as seen in section 8.1 of the the lesson
 	@Override
 	public String toString() {
 		return "There are right decisions, and then there is what you did. Either way your journey is over";
