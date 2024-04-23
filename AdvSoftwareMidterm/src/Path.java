32ed25c2b223ac07e1cd6505b259f6d38ed54a69
JWill-code <willjg24@uwgb.edu>
2024-04-23 16:04:50 -0500

    image saving in eclipse

--- AdvSoftwareMidterm/src/Path.java
+++ AdvSoftwareMidterm/src/Path.java
@@ -14,10 +14,11 @@ public class Path {
 		this.counter = count;
 	}
 
-	Image forest1 = new Image("Images/Jungle Tiger.png");
+	Image building1 = new Image("Images/Abandoned Building.png");
+	Image forest1 = new Image("Images/CreepyForest1.png");
 	Image forest2 = new Image("Images/CreepyForest2.png");
 	Image forest3 = new Image("Images/CreepyForest3.png");
-	Image forest4 = new Image("Images/ForestSnake.png");
+	Image forest4 = new Image("Images/CreepyForest4.png");
 	Image forest5 = new Image("Images/CreepyForest5.png");
 	Image gameOver = new Image("Images/GameOVer.png");
 	Image forest6 = new Image("Images/HauntedForest6.png");
@@ -28,6 +29,23 @@ public class Path {
 	Image road1 = new Image("Images/CreepyRoad.png");
 	Image beginning = new Image("Images/HauntedMaze.png");
 	Image monkey1 = new Image("Images/DesrtMonkey.png");
+	Image bananaBasket = new Image("Images/BannanaDefense.png");
+	Image bananaLoss = new Image("Images/BasketLoss.png");
+	Image snakeLoss = new Image("Images/DanceLoss.png");
+	Image tigerLoss1 = new Image("Images/EatenTiger.png");
+	Image snake1 = new Image("Images/ForestSnake.png");
+	Image hole1 = new Image("Images/Hole.png");
+	Image hole2 = new Image("Images/HoleLoss.png");
+	Image tiger1 = new Image("Images/Jungle Tiger.png");
+	Image steak1 = new Image("Images/NecklaceSteak.png");
+	Image roadLoss = new Image("Images/RoadLoss.png");
+	Image snake2 = new Image("Images/SnakeDance.png");
+	Image snake3 = new Image("Images/SnakeRoad.png");
+	Image snake4 = new Image("Images/SnakeTree.png");
+	Image steakLoss = new Image("Images/SteakLose.png");
+	Image tiger2 = new Image("Images/TigerFight.png");
+	Image treeLoss = new Image("Images/TreeLoss.png");
+	
 	//add images and make sure they upload
 	
 	
